2b9e3ab9aa946dab84108f912b4100a7ac1e932b
Benjamin Stevens <B3nbinoh@gmail.com>
2024-06-24 15:04:13 -0600

    -Added new note page and user input

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const morgan = require("morgan");
 const Note = require("./lib/notes");
+const { body, validationResult } = require("express-validator");
 
 const app = express();
 const host = "localhost";
 const port = "3000";
+let userNotes = [];
+
+userNotes.push(new Note("Note A", "Hello"));
+userNotes.push(new Note("Note B", "Howdy-Doo"));
+userNotes.push(new Note("Note C", "Konichiwa"));
 
 app.set("views", "./views");
 app.set("view engine", "pug");
@@ -17,8 +23,37 @@ app.get("/", (req, res) => {
 });
 
 app.get("/home", (req, res) => {
-  res.render("home");
+  res.render("home", { userNotes });
 });
+
+app.get("/home/new", (req, res) => {
+  res.render("new-note");
+});
+
+app.post(
+  "/home",
+  [
+    body("noteTitle")
+      .trim()
+      .isLength({ min: 0 })
+      .withMessage("Please include a note title.")
+      .isLength({ max: 50 })
+      .withMessage("Title cannot be greater than 50 characters."),
+  ],
+  (req, res, next) => {
+    let errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.render("new-note", {
+        errors: errors.array.map((message) => message.msg),
+      });
+    } else {
+      //FIX-ME need body parsing middlewear to pass form contents into new note
+      userNotes.push(new Note(req.body.noteTitle, req.body.noteContents));
+      res.redirect("/home");
+    }
+  }
+);
 
 app.listen(port, host, () => {
   console.log("Listening on port 3000...");
